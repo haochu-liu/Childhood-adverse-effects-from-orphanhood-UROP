947d36b5bfda514899b1d84fbde6ea076b182809
Haochu <Liu_haochu@outlook.com>
2023-07-03 20:15:03 +0100

    3/7 second

--- try_1.R
+++ try_1.R
@@ -2,10 +2,10 @@ library(rdhs)
 
 
 # setup
-set_rdhs_config(email = "hl3720@ic.ac.uk",
-                project = "Childhood adverse effects from orphanhood",
-                config_path = "rdhs.json",
-                global = FALSE)
+# set_rdhs_config(email = "hl3720@ic.ac.uk",
+#                 project = "Childhood adverse effects from orphanhood",
+#                 config_path = "rdhs.json",
+#                 global = FALSE)
 
 
 # examples
@@ -28,11 +28,23 @@ datasets <- dhs_datasets(surveyIds = "RW2019DHS")
 datasets
 
 # download datasets
-downloads <- get_datasets("RWMR81.ZIP")
-downloads$RWKR21SV
+downloads <- get_datasets("RWKR81SV.ZIP")
+downloads$RWKR81SV
 
 # load datasets into R
-df1 <- readRDS(downloads$RWKR21SV)
+df1 <- readRDS(downloads$RWKR81SV)
+df1 <- data.frame(df1)
 
+# some basic data cleaning
+print(dim(df1))
+colname_list <- colnames(df1)
+na_num <- list()
+for (i in 1:ncol(df1)) {
+  na_num[i] <- sum(is.na(df1[, i]))
+}
+names(na_num) <- colname_list
 
+for (i in 1:length(colname_list)) {
+  if 
+}
 

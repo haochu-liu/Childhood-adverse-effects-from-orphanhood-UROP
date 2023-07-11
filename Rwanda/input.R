9f92c5b7d22e3a42495c3987c44fdd9b7d59f84e
Haochu <Liu_haochu@outlook.com>
2023-07-11 15:10:39 +0100

    11/7

--- Rwanda/input.R
+++ Rwanda/input.R
@@ -14,34 +14,57 @@ datasets
 
 
 # quantify orphanhood
-downloads <- get_datasets("RWHR81DT.ZIP")
-downloads$RWHR81DT # household data
-df_h <- readRDS(downloads$RWHR81DT)
-df_h <- data.frame(df_h)
-
-df <- df_h[, c("hv001", "hv002")]
-df$hv111 <- NA
-df$hv113 <- NA
-
-for (i in 1:nrow(df)) {
-  df_h_111 <- df_h[i,
-                   c("hv111_01", "hv111_02", "hv111_03", "hv111_04", "hv111_05",
-                     "hv111_06", "hv111_07", "hv111_08", "hv111_09", "hv111_10",
-                     "hv111_11", "hv111_12", "hv111_13", "hv111_14", "hv111_15",
-                     "hv111_16")]
-  df_h_113 <- df_h[i,
-                   c("hv113_01", "hv113_02", "hv113_03", "hv113_04", "hv113_05",
-                     "hv113_06", "hv113_07", "hv113_08", "hv113_09", "hv113_10",
-                     "hv113_11", "hv113_12", "hv113_13", "hv113_14", "hv113_15",
-                     "hv113_16")]
-  if (1 %in% df_h_111) {df$hv111[i] <- 1}
-  if (0 %in% df_h_111) {df$hv111[i] <- 0}
-  if (1 %in% df_h_113) {df$hv113[i] <- 1}
-  if (0 %in% df_h_113) {df$hv113[i] <- 0}
-}
+downloads <- get_datasets("RWPR81DT.ZIP")
+downloads$RWPR81DT # household data
+df_p <- readRDS(downloads$RWPR81DT)
+df_p <- data.frame(df_p)
 
 
 # find and combine factors
+df <- df_p[, c("hvidx", "hv001", "hv002", # id
+               "hv111", "hv113", # parents alive
+               "hv014", "hv025", "hv026", "hv201", "hv205", "hv206", "hv208",
+               "hv211", "hv212", "hv217", "hv227", "hv236", "hv243e", "hv270",
+               "chl0", "hv101", "hv104", "hv105", "hv106", "hv107", "hv109",
+               "hv121", "sb332", "ha40", "ha41", "ha50", "ha53", "ha57", "hc1",
+               "hc57", "hb50", "hml35", "sb240", "sb121a", "sb121b", "sb121c",
+               "sb121d", "sb121e", "sb121f", "sb121g", "sb121h", "sb124",
+               "sb129", "mha40", "mha41", "mha50", "mha57", "sb533", "sb421",
+               "sb422a", "sb422b", "sb422c", "sb422d", "sb422e", "sb422f",
+               "sb422g", "sb422h", "sb425", "sb430")]
+
+
+# cleaning
+df <- df[, colSums(is.na(df)) < nrow(df)]
+# under 18
+under_18 <- c(sum(df$hv105 < 18), sum(df$hv105 => 18))
+x_axis <- c("< 18", ">= 18")
+barplot(under_18, names.arg=x_axis, xlab="Age", ylab="Number of individuals",
+        col="blue", main="Adults in Rwanda")
+df <- df[df$hv105 < 18, ]
+# set age as categorical
+df$age_group <- NA
+for (i in 1:nrow(df)) {
+  if (df$hv105 < 5) {df$age_group <- 1} # age 0-4
+  if (df$hv105 <= 9 & df$hv105 >= 5) {df$age_group <- 2} # age 5-9
+  if (df$hv105 <= 14 & df$hv105 >= 10) {df$age_group <- 3} # age 10-14
+  if (df$hv105 <= 17 & df$hv105 >= 15) {df$age_group <- 4} # age 15-17
+}
+
+# count missing data
+colname_list <- colnames(df)
+na_num <- list()
+for (i in 1:ncol(df)) {
+  na_num[i] <- sum(is.na(df[, i]))
+}
+names(na_num) <- colname_list
+# drop columns with a high proportion of NA
+df <- df[, 1:24]
+# drop rows with NA in mother/father alive
+df <- df[complete.cases(df[, 4:5]), ]
+
+
+
 
 
 

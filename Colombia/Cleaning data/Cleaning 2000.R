2ee4f55f6d5316d2635a8a923cd971f14f06cb7c
Xialu <zhengxialu@gmail.com>
2023-07-21 13:54:49 +0100

    hi

---
+++ Colombia/Cleaning data/Cleaning 2000.R
@@ -0,0 +1,55 @@
+# 2000
+df2000_new <- df2000[, c("hvidx","hv001","hv002",
+                         "hv111","hv113", 
+                         "hv217","hv105","hv104",
+                         "ha3","hc3","ha2","hc2",
+                         "hv025","hv026",
+                         "hv201","hv205","hv206","hv207","hv208","hv209","hv221",
+                         "hv106","hv107","hv121","hv121"
+)]
+# orphanhood
+df2000_new["Orphanhood"] <- ifelse(df2000$hv111 == 0 | df2000$hv113 == 0, 
+                                   "orphan", "non-orphan")
+
+# under 18
+under_18_2000 <- c(sum(df2000_new$hv105 < 18,na.rm=TRUE), sum(df2000_new$hv105 >= 18,na.rm=TRUE))
+barplot(under_18_2000, names.arg= c("< 18", ">= 18"), xlab="Age", ylab="Number of individuals",
+        col="blue", main= "Colombia2000")
+
+df2000_new <- df2000_new[df2000_new$hv105 < 18, ]
+
+# labels
+library(Hmisc)
+label <- as.list(label(df2000_new))
+label$hv025 <- "Lives in urban area" # 1: urban, 2: rural
+label$hv201 <- "Has piped or tube water" # <=40: pipe/well 71: bottled
+label$hv205 <- "Has flush or pit toilet" # <=21: flush/pit, >21: not have
+label$hv121 <- "School attendance for age 5-16 (compulsory)" # 0: no, >=1: attend
+label$hv121.1 <- "School attendance for age 17"
+
+label(df2000_new) <- label
+
+# clean columns
+df2000_new$hv104[df2000_new$hv104 > 2] <- NA
+
+df2000_new$hv025 <- ifelse(df2000_new$hv025 == 1, 1, 0)
+
+df2000_new$hv201[df2000_new$hv201 == 99] <- NA
+df2000_new$hv201 <- ifelse(df2000_new$hv201 <= 71, 1, 0)
+
+df2000_new$hv205[df2000_new$hv205 > 96] <- NA
+df2000_new$hv205 <- ifelse(df2000_new$hv205 < 40 | df2000_new$hv205 == 71, 1, 0) 
+
+df2000_new$hv206[df2000_new$hv206 == 9] <- NA
+df2000_new$hv207[df2000_new$hv207 == 9] <- NA
+df2000_new$hv208[df2000_new$hv208 == 9] <- NA
+df2000_new$hv209[df2000_new$hv209 == 9] <- NA
+df2000_new$hv221[df2000_new$hv221 == 9] <- NA
+
+df2000_new$hv121[df2000_new$hv121 == 9] <- NA
+df2000_new$hv121[df2000_new$hv105 < 5 | df2000_new$hv105 > 16] <- NA
+df2000_new$hv121 <- ifelse(df2000_new$hv121 >= 1, 1, 0)
+
+df2000_new$hv121.1[df2000_new$hv121.1 == 9] <- NA
+df2000_new$hv121.1[df2000_new$hv105 == 17] <- NA
+df2000_new$hv121.1 <- ifelse(df2000_new$hv121.1 >= 1, 1, 0)
--- Colombia/Orphanhood.R
+++ Colombia/Orphanhood.R
@@ -7,13 +7,64 @@ set_rdhs_config(email = "zhengxialu@gmail.com",
 
 
 # getting data
-data1 <- get_datasets(dataset_filenames = "COHR72DT.zip")
-df1 <- readRDS(data1$"COHR72DT")
-df1 <- data.frame(df1)
+data2015 <- get_datasets(dataset_filenames = "COPR72DT.zip")
+df2015 <- readRDS(data2015$"COPR72DT")
+df2015 <- data.frame(df2015)
 
-data2 <- get_datasets(dataset_filenames = "COKR72DT.zip")
-df2 <- readRDS(data2$"COKR72DT")
-df2 <- data.frame(df2)
+# gathering useful infor in one dataframe
+df <- df2015[, c("hhid","hvidx","hv001","hv002","hv003")] #identify respondent
+df["hv111"] <- df2015$hv111 #whether mother alive
+df["hv113"] <- df2015$hv113 #whether father alive
 
-# collecting data
-df <- df1[, c("v001", "v002")]
+df["age"] <- df2015$hv105
+df["hv121"] <- df2015$hv121
+df["hv104"] <- df2015$hv104
+df["hv110"] <- df2015$hv110
+
+na.omit(df)
+
+View(df)
+
+library(haven)
+opdf <- subset(df, df$hv111 == 0 | df$hv113 == 0)
+nopdf <- subset(df, df$hv111 != 0 & df$hv113 != 0)
+lmdf <- subset(df, df$hv111 == 0)
+lfdf <- subset(df, df$hv113 == 0)
+
+# children
+chdf <- subset(df, df$age > 6 & df$age < 15)
+
+# gender hv104
+fdf<-subset(chdf,chdf$hv104==2)
+mdf<-subset(chdf,chdf$hv104==1)
+fopdf<-subset(opdf,opdf$hv104==2)
+mopdf<-subset(opdf,opdf$hv104==1)
+
+# HV121 Member attended school during current school year
+ch1<-table(chdf$hv121)[2]/nrow(chdf)
+op1<-table(opdf$hv121)[2]/nrow(opdf)
+nop1<-table(nopdf$hv121)[2]/nrow(nopdf)
+
+lm1<-table(lmdf$hv121)[2]/nrow(lmdf)
+lf1<-table(lfdf$hv121)[2]/nrow(lfdf)
+
+f1<-table(fdf$hv121)[2]/nrow(fdf)
+m1<-table(mdf$hv121)[2]/nrow(mdf)
+
+fop1<-table(fopdf$hv121)[2]/nrow(fopdf)
+mop1<-table(mopdf$hv121)[2]/nrow(mopdf)
+
+hv121vec<-c(op1,nop1,lm1,lf1)
+#draw a barplot of hv121 statistics
+par(mar=c(3,3,3,3))
+barplot(hv121vec,names.arg=c("op1","nop1","lm1","lf1"),main="hv121 chart",xlab="class",ylab="proportion",xlim=c(0,4),ylim=c(0,1))
+
+hv121vec<-c(ch1,f1,m1)
+#draw a barplot of hv121 statistics
+par(mar=c(3,3,3,3))
+barplot(hv121vec,names.arg=c("ch1","f1","m1"),main="hv121 chart",xlab="class",ylab="proportion",xlim=c(0,4),ylim=c(0,1))
+
+hv121vec<-c(fop1,mop1,op1)
+#draw a barplot of hv121 statistics
+par(mar=c(3,3,3,3))
+barplot(hv121vec,names.arg=c("fop1","mop1","op1"),main="hv121 chart",xlab="class",ylab="proportion",xlim=c(0,4),ylim=c(0,1))

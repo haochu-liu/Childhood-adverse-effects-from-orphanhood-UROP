5be7a0494d1fdd5a6fec6f01e1d7b42a885a1fe4
Haochu <Liu_haochu@outlook.com>
2023-07-23 11:38:24 +0100

    SUNDAY!

--- Rwanda/visualisation.R
+++ Rwanda/visualisation.R
@@ -109,6 +109,361 @@ ggplot(box_df, aes(x=Orphanhood, y=hc53)) +
   coord_flip()
 
 
+# 2014
+year <- 2014
+load("Rwanda/df_2014.Rda")
+bar_col <- c("hv025", "hv201", "hv205", "hv206",
+             "hv207", "hv208", "hv209", "hv210",
+             "hv211", "hv212", "hv227", "hv221",
+             "hv243a", "hv243b", "hv270",
+             "hv121", "hv121.1", "ha57", "hc57", "hml32")
+box_col <- c("ha3", "hc3", "hb3", "ha2", "hc2", "hb2", "ha40", "hb40", "ha53", "hc53")
+bar_df <- df_barplot(df_2014, bar_col, "Orphanhood")
+box_df <- df_boxplot(df_2014, box_col, "Orphanhood", "hv105")
+# barplot
+ggplot(bar_df, aes(fill=orphan, x=column_labels, y=percentage)) +
+  geom_col(width=0.5, position=position_dodge(0.5)) +
+  geom_errorbar(aes(ymin=CI_lower, ymax=CI_upper),
+                width=0.4, colour="black", position = position_dodge(.5)) +
+  labs(x = "Questions") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip(ylim=c(0, 1)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_classic()
+# boxplot
+box_labels <- label(box_df)
+val_labels(box_df) <- NULL
+
+ggplot(box_df, aes(x=hv105, y=ha3, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["ha3"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hc3, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hc3"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hb3, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hb3"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=ha2, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["ha2"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hc2, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hc2"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hb2, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hb2"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=ha40)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["ha40"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=hb40)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["hb40"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=ha53)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["ha53"])) +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=hc53)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["hc53"])) +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+
+# 2010
+year <- 2010
+load("Rwanda/df_2010.Rda")
+bar_col <- c("hv025", "hv201", "hv205", "hv206",
+             "hv207", "hv208", "hv209", "hv210",
+             "hv211", "hv212", "hv227", "hv221",
+             "hv243a", "hv243b", "hv270",
+             "hv121", "hv121.1", "ha57", "hc57", "hml32")
+box_col <- c("ha3", "hc3", "hb3", "ha2", "hc2", "hb2", "ha40", "hb40", "ha53", "hc53")
+bar_df <- df_barplot(df_2010, bar_col, "Orphanhood")
+box_df <- df_boxplot(df_2010, box_col, "Orphanhood", "hv105")
+# barplot
+ggplot(bar_df, aes(fill=orphan, x=column_labels, y=percentage)) +
+  geom_col(width=0.5, position=position_dodge(0.5)) +
+  geom_errorbar(aes(ymin=CI_lower, ymax=CI_upper),
+                width=0.4, colour="black", position = position_dodge(.5)) +
+  labs(x = "Questions") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip(ylim=c(0, 1)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_classic()
+# boxplot
+box_labels <- label(box_df)
+val_labels(box_df) <- NULL
+
+ggplot(box_df, aes(x=hv105, y=ha3, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["ha3"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hc3, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hc3"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hb3, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hb3"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=ha2, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["ha2"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hc2, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hc2"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hb2, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hb2"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=ha40)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["ha40"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=hb40)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["hb40"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=ha53)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["ha53"])) +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=hc53)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["hc53"])) +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+
+# 2005
+year <- 2005
+load("Rwanda/df_2005.Rda")
+bar_col <- c("hv025", "hv201", "hv205", "hv206",
+             "hv207", "hv208", "hv209", "hv210",
+             "hv211", "hv212", "hv227", "hv221",
+             "hv270",
+             "hv121", "hv121.1", "ha57", "hc57")
+box_col <- c("ha3", "hc3", "ha2", "hc2", "ha40", "ha53", "hc53")
+bar_df <- df_barplot(df_2005, bar_col, "Orphanhood")
+box_df <- df_boxplot(df_2005, box_col, "Orphanhood", "hv105")
+# barplot
+ggplot(bar_df, aes(fill=orphan, x=column_labels, y=percentage)) +
+  geom_col(width=0.5, position=position_dodge(0.5)) +
+  geom_errorbar(aes(ymin=CI_lower, ymax=CI_upper),
+                width=0.4, colour="black", position = position_dodge(.5)) +
+  labs(x = "Questions") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip(ylim=c(0, 1)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_classic()
+# boxplot
+box_labels <- label(box_df)
+val_labels(box_df) <- NULL
+
+ggplot(box_df, aes(x=hv105, y=ha3, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["ha3"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hc3, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hc3"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=ha2, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["ha2"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=hv105, y=hc2, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hc2"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=ha40)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["ha40"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=ha53)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["ha53"])) +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+ggplot(box_df, aes(x=Orphanhood, y=hc53)) +
+  geom_violin(aes(fill=Orphanhood), width=0.8, alpha=0.5) +
+  geom_boxplot(width=0.1) +
+  labs(y = as.character(box_labels["hc53"])) +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+
+# 2000
+year <- 2000
+load("Rwanda/df_2000.Rda")
+bar_col <- c("hv025", "hv201", "hv205", "hv206",
+             "hv207", "hv208", "hv209", "hv210",
+             "hv211", "hv212", "hv227", "hv221",
+             "hv121", "hv121.1", "ha57", "hc57")
+box_col <- c("ha3", "hc3", "ha2", "hc2", "ha40", "ha53", "hc53")
+bar_df <- df_barplot(df_2000, bar_col, "Orphanhood")
+box_df <- df_boxplot(df_2000, box_col, "Orphanhood", "hv105")
+# barplot
+ggplot(bar_df, aes(fill=orphan, x=column_labels, y=percentage)) +
+  geom_col(width=0.5, position=position_dodge(0.5)) +
+  geom_errorbar(aes(ymin=CI_lower, ymax=CI_upper),
+                width=0.4, colour="black", position = position_dodge(.5)) +
+  labs(x = "Questions") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip(ylim=c(0, 1)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_classic()
+# boxplot
+box_labels <- label(box_df)
+val_labels(box_df) <- NULL
+
+sum(is.na(box_df$ha3)) == nrow(box_df)
+
+ggplot(box_df, aes(x=hv105, y=hc3, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hc3"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+sum(is.na(box_df$ha2)) == nrow(box_df)
+
+ggplot(box_df, aes(x=hv105, y=hc2, fill=Orphanhood)) +
+  geom_violin(aes(fill=Orphanhood),
+              width=0.8, alpha=0.5, position = position_dodge(0.9)) +
+  geom_boxplot(width=0.2, position = position_dodge(0.9)) +
+  labs(y = as.character(box_labels["hc2"]), x = "Age") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip()
+
+sum(is.na(box_df$ha40)) == nrow(box_df)
+
+sum(is.na(box_df$ha53)) == nrow(box_df)
+
+sum(is.na(box_df$hc53)) == nrow(box_df)
+
+
+# 1992
+year <- 1992
+load("Rwanda/df_1992.Rda")
+bar_col <- c("hv025", "hv201", "hv205", "hv206",
+             "hv207", "hv208", "hv209", "hv210",
+             "hv211", "hv212",
+             "hv110", "hv110.1")
+bar_df <- df_barplot(df_1992, bar_col, "Orphanhood")
+# barplot
+ggplot(bar_df, aes(fill=orphan, x=column_labels, y=percentage)) +
+  geom_col(width=0.5, position=position_dodge(0.5)) +
+  geom_errorbar(aes(ymin=CI_lower, ymax=CI_upper),
+                width=0.4, colour="black", position = position_dodge(.5)) +
+  labs(x = "Questions") +
+  ggtitle(paste("Rwanda", year)) +
+  coord_flip(ylim=c(0, 1)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_classic()
+
+
+
 
 
 

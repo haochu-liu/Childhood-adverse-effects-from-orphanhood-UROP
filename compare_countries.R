552eaf336f180ed22ccc0903694a213a59c4fcc5
Haochu <Liu_haochu@outlook.com>
2023-07-27 15:51:09 +0100

    27/7

--- compare_countries.R
+++ compare_countries.R
@@ -8,7 +8,12 @@ library(patchwork)
 library(scales)
 
 
-load()
+load("Rwanda/bar_RW_2019.Rda")
+load("Rwanda/odd_RW_2019.Rda")
+load("Colombia/bar_CO_2015.Rda")
+load("Colombia/odd_CO_2015.Rda")
+load("Senegal/bar_SN_2019.Rda")
+load("Senegal/odd_SN_2019.Rda")
 
 
 

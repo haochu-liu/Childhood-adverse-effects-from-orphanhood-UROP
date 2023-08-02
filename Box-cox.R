8b35d31fba67a769da3f913d6b4799dd68ebafb5
Xialu <zhengxialu@gmail.com>
2023-08-02 14:48:56 +0100

    hi

--- Box-cox.R
+++ Box-cox.R
@@ -1,34 +1,65 @@
 library(MASS)
-# assume normal and same variance
+library(labelled)
 
-fieller_child_df<-function(df,country,year){
+# Fieller theorem assumes independent normal and same variance
+
+boxcox_trans <- function(vector, lambda){
+  if (lambda == 0) {
+    vector <- log(vector)
+  } else {
+    vector <- (vector^lambda - 1) / lambda
+  }
+  
+  vector
+}
+
+fieller_child_df <- function(df,country,year){
   cont_df <- subset(df, select = c("hc3", "hc2", "hv105", "Orphanhood"))
   cont_df <- na.omit(cont_df)
+  
+  val_labels(cont_df) <- NULL  
   
   fieller_df <- data.frame(matrix(ncol=7,nrow=0))
   colnames(fieller_df)<-c("col_labels","ratio","upper","lower","p-value","country","year")
   
-  # height
-  hmodel <- lm(hc3~1, data = cont_df)
-  bc_h <- boxcox(hmodel, lambda = seq(-5, 5, 0.2))
-  # weight
-  wmodel <- lm(hc2~1, data = cont_df)
-  bc_w <- boxcox(wmodel, lambda = seq(-5, 5, 0.2))
+  # 0-2 and 2-5 year groups 
+  yrdf1 <- subset(cont_df, cont_df$hv105 < 2)
+  yrdf2 <- subset(cont_df, cont_df$hv105 >= 2)
+
+  # boxcox to find 4 lambdas
+  hmodel1 <- lm(hc3 ~ 1, data = yrdf1)
+  bc_h1 <- boxcox(hmodel1, lambda = seq(-5, 5, 0.2))
+  print(head(yrdf1))
+  
+  hmodel2 <- lm(hc3 ~ 1, data = yrdf2)
+  bc_h2 <- boxcox(hmodel2, lambda = seq(-5, 5, 0.2))
+  
+  wmodel1 <- lm(hc2 ~ 1, data = yrdf1)
+  bc_w1 <- boxcox(wmodel1, lambda = seq(-5, 5, 0.2))
+  
+  wmodel2 <- lm(hc2 ~ 1, data = yrdf2)
+  bc_w2 <- boxcox(wmodel2, lambda = seq(-5, 5, 0.2))
   
   # find lambda
-  lambda_h <- round(bc_h$x[which.max(bc_h$y)])
-  lambda_w <- round(bc_w$x[which.max(bc_w$y)])
+  lambda_h1 <- round(bc_h1$x[which.max(bc_h1$y)])
+  lambda_h2 <- round(bc_h2$x[which.max(bc_h2$y)])
+  lambda_w1 <- round(bc_w1$x[which.max(bc_w1$y)])
+  lambda_w2 <- round(bc_w2$x[which.max(bc_w2$y)])
   
   # transform
-  cont_df["bc_height"] <- (cont_df$hc3^lambda_h - 1) / lambda_h
-  cont_df["bc_weight"] <- (cont_df$hc2^lambda_w - 1) / lambda_w
+  yrdf1["bc_height"] <- (yrdf1$hc3^lambda_h1 - 1) / lambda_h1
+  yrdf1["bc_weight"] <- (yrdf1$hc2^lambda_w1 - 1) / lambda_w1
+  
+  yrdf2["bc_height"] <- (yrdf2$hc3^lambda_h2 - 1) / lambda_h2
+  yrdf2["bc_weight"] <- (yrdf2$hc2^lambda_w2 - 1) / lambda_w2
   
   #orphan/nonorphan datasets in 0-2 and 2-5 year groups
-  ordf1 <- subset(cont_df, cont_df$Orphanhood=="orphan" & cont_df$hv105 < 2)
-  nordf1 <- subset(cont_df, cont_df$Orphanhood=="non-orphan" & cont_df$hv105 < 2)
-  ordf2 <- subset(cont_df, cont_df$Orphanhood=="orphan" & cont_df$hv105 >= 2)
-  nordf2 <- subset(cont_df, cont_df$Orphanhood=="non-orphan" & cont_df$hv105 >= 2)
+  ordf1 <- subset(yrdf1, cont_df$Orphanhood=="orphan")
+  nordf1 <- subset(yrdf1, cont_df$Orphanhood=="non-orphan")
   
+  ordf2 <- subset(yrdf2, cont_df$Orphanhood=="orphan")
+  nordf2 <- subset(yrdf2, cont_df$Orphanhood=="non-orphan")
+
   # Fieller
   ttr1_h <- ttestratio(ordf1$bc_height, nordf1$bc_height, var.equal=TRUE, conf.level=0.95)
   ttr2_h <- ttestratio(ordf2$bc_height, nordf2$bc_height, var.equal=TRUE, conf.level=0.95)

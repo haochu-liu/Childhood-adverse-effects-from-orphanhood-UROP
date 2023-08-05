adef7182a7d5adc1fd9c6e01441b1e00240b8044
Haochu <Liu_haochu@outlook.com>
2023-08-05 15:00:43 +0100

    5/8

--- Rwanda/table_RW.R
+++ Rwanda/table_RW.R
@@ -9,14 +9,11 @@ df_2019 <- df_2019[, col_names]
 df_2014 <- df_2014[, col_names]
 df_2010 <- df_2010[, col_names]
 df_2005 <- df_2005[, col_names]
-df_2019 <- na.omit(df_2019)
-df_2014 <- na.omit(df_2014)
-df_2010 <- na.omit(df_2010)
-df_2005 <- na.omit(df_2005)
 val_labels(df_2019) <- NULL
 val_labels(df_2014) <- NULL
 val_labels(df_2010) <- NULL
 val_labels(df_2005) <- NULL
+
 
 year <- c("Children under 18", "Children aged 0-4 years",
           "Children aged 5-15 years", "Children aged 16-18 years")
@@ -56,4 +53,22 @@ df_orphan$"2005" <- c(sum(df_2005$hv105<18 & df_2005$Orphanhood=="orphan"),
                       sum(df_2005$hv105<5 & df_2005$Orphanhood=="orphan"),
                       sum(df_2005$hv105>=5 & df_2005$hv105<16 & df_2005$Orphanhood=="orphan"),
                       sum(df_2005$hv105>=16 & df_2005$hv105<18 & df_2005$Orphanhood=="orphan"))
+
+
+year <- c("household", "household with orphan")
+df_household <- data.frame(year)
+df_2019$Orphanhood[df_2019$Orphanhood == "non-orphan"] <- NA
+df_household$"2019" <- c(nrow(df_2019 %>% distinct(hv001, hv002, .keep_all = TRUE)),
+                         nrow(na.omit(df_2019) %>% distinct(hv001, hv002, .keep_all = TRUE)))
+df_2014$Orphanhood[df_2014$Orphanhood == "non-orphan"] <- NA
+df_household$"2014" <- c(nrow(df_2014 %>% distinct(hv001, hv002, .keep_all = TRUE)),
+                         nrow(na.omit(df_2014) %>% distinct(hv001, hv002, .keep_all = TRUE)))
+df_2010$Orphanhood[df_2010$Orphanhood == "non-orphan"] <- NA
+df_household$"2010" <- c(nrow(df_2010 %>% distinct(hv001, hv002, .keep_all = TRUE)),
+                         nrow(na.omit(df_2010) %>% distinct(hv001, hv002, .keep_all = TRUE)))
+df_2005$Orphanhood[df_2005$Orphanhood == "non-orphan"] <- NA
+df_household$"2005" <- c(nrow(df_2005 %>% distinct(hv001, hv002, .keep_all = TRUE)),
+                         nrow(na.omit(df_2005) %>% distinct(hv001, hv002, .keep_all = TRUE)))
+
+
 

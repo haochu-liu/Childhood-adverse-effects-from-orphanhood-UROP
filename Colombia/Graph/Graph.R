a9f9812d69e873497e9930893122063188797b4e
Xialu <zhengxialu@gmail.com>
2023-08-07 16:40:37 +0100

    hi

--- Colombia/Graph/Graph.R
+++ Colombia/Graph/Graph.R
@@ -190,7 +190,7 @@ ggsave("bar_CO_items.png",
        height = 5.6, width = 8.5, dpi = 700)
 
 # fieller
-source("~/Desktop/Childhood-adverse-effects-from-orphanhood-UROP/Box-cox.R")
+source("~/Desktop/Childhood-adverse-effects-from-orphanhood-UROP/functions_for_plotting.R")
 library(forester)
 
 fieller_CO1 <- fieller_child_df(df2005_new, "Colombia", "2005")
@@ -220,6 +220,37 @@ forester(left_side_data = fieller_CO[,c("Outcome","p_value","parameter"), drop=F
          estimate_col_name = "Ratio with 95% CI",
          file_path = here::here("Colombia/fieller_plot_CO.png"), 
          render_as = "png")
+
+library(MASS)
+library(labelled)
+library(mratios)
+library(ggplot2)
+library(forester)
+
+# weight (woman): lambda = 0
+# height (woman): lambda = 2
+
+# weight (age 0-2): lambda = 1
+# weight (age 2-5): lambda = 0
+# height (age 0-2): lambda = 2
+# height (age 2-5): : lambda = 1
+
+lambda_w = 0
+lambda_h = 2
+
+lambda_w1 = 1
+lambda_w2 = 0
+lambda_h1 = 2
+lambda_h2 = 1
+
+df_woman <- rbind(df_woman_lfix(df2005_new, "Colombia", "2005", lambda_h, lambda_w),
+                  df_woman_lfix(df2010_new, "Colombia", "2010", lambda_h, lambda_w))
+
+df_child <- rbind(df_child_lfix(df2005_new, "Colombia", "2005", lambda_h1, lambda_h2, lambda_w1, lambda_w2),
+                  df_child_lfix(df2010_new, "Colombia", "2010", lambda_h1, lambda_h2, lambda_w1, lambda_w2))
+
+fieller_fix_CO <- rbind(df_woman, df_child)
+save(fieller_fix_CO, file = "Colombia/fieller_fix_CO.Rda")
 
 
 # education odd ratio plot
